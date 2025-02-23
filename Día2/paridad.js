a32bfc590daf72788560c288ffe4ad13e179c82f
= <=>
2025-02-22 23:09:47 -0600

    feat: Agrega solucion dia 2, funcion que verifica si es par o impar, utilizando condicional if y operador ternario

--- Día2/paridad.js
+++ Día2/paridad.js
@@ -8,6 +8,21 @@ const esPar = (num) => {
     : num % 2 === 0 ? true : false;
 };
 
-console.log(esPar(4)); // true
-console.log(esPar(7)); // false
-console.log(esPar("Hola")); // Error ingresa un número
+// Declara una funcion que recibe un parametro
+function esPar2(num){
+    // Valida si el dato es de tipo numero
+    if( typeof num === 'number'){
+        // Valida si es par o impar
+        return num % 2 === 0 ? true : false
+    }
+    // Si no es de tipo numero retorna un error
+    return 'Error ingresa un valor de tipo numero'
+}
+
+//console.log(esPar(4)); // true
+//console.log(esPar("Hola")); // Error ingresa un número
+//console.log(esPar(7)); // false
+
+console.log(esPar2(4)); // true
+console.log(esPar2(7)); // false
+console.log(esPar2("Numero")); // Error ingresa un numero
